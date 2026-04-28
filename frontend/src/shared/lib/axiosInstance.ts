7a4721791d07e713837a0b99450952be55c5101a
Susik09 <kirasuslova702@gmail.com>
2026-04-28 11:37:47 +0300

    add axios instance

--- frontend/src/shared/lib/axiosInstance.ts
+++ frontend/src/shared/lib/axiosInstance.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: ''
+    baseURL: import.meta.env.BASE_URL || 'http://localhost:3009',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+axiosInstance.interceptors.request.use(function (config) {
+    const token = 'token'
+    if (token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
 })
